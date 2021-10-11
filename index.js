37d93e44d097529175afbc516823a1c5afcadc1b
ant_reyn <antreyn@udel.edu>
2021-10-11 17:58:29 -0400

    added new card option and improved the startGame function

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-let firstCard = 11;
-let secondCard = 10;
-
+const messageEl = document.querySelector("#message-el");
+const sumEl = document.querySelector("#sum-el");
+const cardsEl = document.querySelector("#cards-el");
+let firstCard = 15;
+let secondCard = 1;
+let totalCards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
-} else if (sum === 21) {
-  console.log("Woohoo! You've got blackjack!");
-} else {
-  console.log("You're out of the game!");
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = `Cards: ${totalCards[0]} and ${totalCards[1]}`;
+  sumEl.textContent = "Sum: " + sum;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game.";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let newCard = 5;
+  sum += newCard;
+  renderGame();
 }
